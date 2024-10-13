ba9731a24aa11598b867675d73d10cf8a7ffc4ba
miftahul21 <miftajanahhul@gmail.com>
2024-10-13 22:17:10 +0800

    Add files via upload

--- cetak_info.php
+++ cetak_info.php
@@ -1,7 +1,7 @@
 <?php
    require_once('../../_functions.php');
-   $id_cs = $_GET['id_cs'];
-   $data = query("SELECT * FROM tb_riwayat_cs WHERE id_cs = '$id_cs'")[0];
+   $id_dc = $_GET['id_dc'];
+   $data = query("SELECT * FROM tb_riwayat_dc WHERE id_dc = '$id_dc'")[0];
    // var_dump($data);
 ?>
 <!DOCTYPE html>
@@ -56,14 +56,14 @@
 
                <table class="tb_byr">
                   <tr>
-                     <th class="tb_heading">Jenis</th>
-                     <th class="tb_heading">Jumlah (Pcs)</th>
+                     <th class="tb_heading">Jenis paket</th>
+                     <th class="tb_heading">Berat (Kg)</th>
                      <th class="tb_heading">Harga Per-kilo</th>
                   </tr>
                   <tr>
                      <td><?=$data['j_paket']?></td>
-                     <td><?=$data['jml_pcs'] . " Pcs"?></td>
-                     <td><?="Rp. " . $data['h_perpcs'] . " x " . $data['jml_pcs']?></td>
+                     <td><?=$data['berat'] . " Kg"?></td>
+                     <td><?="Rp. " . $data['h_perkilo'] . " x " . $data['berat']?></td>
                   </tr>
                   <tr>
                      <th colspan="2" class="ub">Total</th>
